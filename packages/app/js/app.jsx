1327afbe613f643f7715f6f501e0bf3f639a3d08
Eric Fung <ericf@carbonfive.com>
2018-03-31 13:30:24 -0700

    Import components from webpacked library

--- packages/app/js/app.jsx
+++ packages/app/js/app.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HelloWorld } from "@bundler-comparison/components-webpack";
 
-ReactDOM.render(
-  <h1>Hello World!</h1>,
-  document.getElementById("root")
-);
+ReactDOM.render(<HelloWorld/>, document.getElementById("root"));
