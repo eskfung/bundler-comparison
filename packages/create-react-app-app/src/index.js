287392f99546cedffc6a3dd35a36044eed3272b5
Eric Fung <ericf@carbonfive.com>
2018-03-31 18:09:07 -0700

    Boostrap a create-react-app app

---
+++ packages/create-react-app-app/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { HelloWorld } from "@bundler-comparison/components-webpack";
+// import { HelloWorld } from "@bundler-comparison/components-rollup";
+
+ReactDOM.render(<HelloWorld />, document.getElementById("root"));
