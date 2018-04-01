8900a9b2feef108208e630c48c88d707972cff1b
Eric Fung <ericf@carbonfive.com>
2018-04-01 16:05:20 -0700

    Standardize configs and organization across packages

--- packages/create-react-app-app/src/index.js
+++ packages/create-react-app-app/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HelloWorld } from "@bundler-comparison/components-webpack";
-// import { HelloWorld } from "@bundler-comparison/components-rollup";
+// import { HelloWorld } from "@bundler-comparison/components-babel";
+import { HelloWorld } from "@bundler-comparison/components-rollup";
+// import { HelloWorld } from "@bundler-comparison/components-webpack";
 
 ReactDOM.render(<HelloWorld />, document.getElementById("root"));
--- packages/custom-webpacked-app/src/index.js
+++ packages/custom-webpacked-app/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import { HelloWorld } from "@bundler-comparison/components-babel";
+import { HelloWorld } from "@bundler-comparison/components-rollup";
 // import { HelloWorld } from "@bundler-comparison/components-webpack";
-// import { HelloWorld } from "@bundler-comparison/components-rollup";
-import { HelloWorld } from "@bundler-comparison/components-babel";
 
 ReactDOM.render(<HelloWorld />, document.getElementById("root"));
